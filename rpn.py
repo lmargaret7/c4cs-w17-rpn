e077cf0bc1dd01cbc03129da4b7ef10765f86aef
laura <huangla@umich.edu>
2017-03-24 02:34:26 -0400

    add code

--- rpn.py
+++ rpn.py
@@ -24,10 +24,27 @@ def calculate(arg):
 			
 			stack.append(result)
 	return stack.pop()
+def calculate_3(arg):
+	stack = list()
+	for operand in arg.split():
+		try:
+			operand = float(operand)
+			stack.append(operand)
+		except:
+			arg3 = stack.pop()
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			operator_fn = OPERATORS[operand]
+			int_result = operator_fn(arg1, arg2)
+			fin_result = operator_fn(int_result, arg3)
+			
+			stack.append(fin_result)
+	return stack.pop()
 
 def main():
 	while True:
-		result = calculate(input('rpn calc> '))
+		#result = calculate(input('rpn calc> '))
+		result = calculate_3(input('rpn calc_3> '))
 		print("Result:", result)
 
 if __name__ == '__main__':
